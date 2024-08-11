35f24b3ce2e973821f9060b45b2b1a13ecebb99c
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-08-12 00:09:27 +0530

    set fetching to client side for responsiveness

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,12 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import CardsTable from "./_components/cards-table";
 import { Card } from "@prisma/client";
 
 async function fetchFlashcards() {
   try {
-    const res = await fetch("https://tuf-flashcards.vercel.app/api/cards", {
-      next: { revalidate: 10 },
-    });
+    const res = await fetch("/api/cards");
     if (!res.ok) {
       return [];
     } else {
@@ -17,8 +18,27 @@ async function fetchFlashcards() {
   }
 }
 
-export default async function AdminPage() {
-  const cards = await fetchFlashcards();
+export default function AdminPage() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedCards = await fetchFlashcards();
+      setCards(fetchedCards);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex h-full items-center justify-center text-xl">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <main className="p-6">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { Card } from "@prisma/client";
 import DisplayCards from "./_components/display-cards";
 
 async function fetchFlashcards() {
   try {
-    const res = await fetch("https://tuf-flashcards.vercel.app/api/cards", {
-      next: { revalidate: 10 },
-    });
+    const res = await fetch("/api/cards");
     if (!res.ok) {
       return [];
     } else {
@@ -17,8 +18,27 @@ async function fetchFlashcards() {
   }
 }
 
-export default async function Home() {
-  const cards = await fetchFlashcards();
+export default function Home() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedCards = await fetchFlashcards();
+      setCards(fetchedCards);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex h-full items-center justify-center bg-[#d5fcd5] text-xl">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <main className="flex-1">
